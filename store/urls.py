fe66b2b4e62f54e40133957c15c99d0c68463aa1
Philani_Antony <philaniantonydlamini@gmail.com>
2020-07-15 12:14:16 +0200

    First Commit

---
+++ store/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	#Leave as empty string for base url
+	path('', views.store_view, name="store"),
+	path('cart/', views.cart_view, name="cart"),
+	path('checkout/', views.checkout_view, name="checkout"),
+
+	#path('update_item/', views.updateitem_view, name="updateitem"),
+	#path('process_order/', views.processorder_view, name="processorder"),
+
+]
