945d2ab0250b34bdec76c35e7cca70416f3f00db
Philani_Antony <philaniantonydlamini@gmail.com>
2020-07-20 20:08:41 +0200

    Second Commit

--- store/urls.py
+++ store/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
 	path('cart/', views.cart_view, name="cart"),
 	path('checkout/', views.checkout_view, name="checkout"),
 
-	#path('update_item/', views.updateitem_view, name="updateitem"),
+	path('update_item/', views.updateitem_view, name="updateitem"),
 	#path('process_order/', views.processorder_view, name="processorder"),
 
 ]
--- store/views.py
+++ store/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
+
 
 def store_view(request):
+    if request.user.is_authenticated :
+        customer = request.user.customer
+        order,created = Order.objects.get_or_create(customer= customer, complete= False)
+        items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
+    else:
+        item = []
+        order = {'get_cart_total': 0, 'get_cart_items': 0}
+        cartitems =order['get_cart_items']
     products = Product.objects.all()
-    context = {'products': products,}
+    context = {'products': products,
+               'cartitems': cartitems}
     return render(request, 'store/store.html', context)
 
 
@@ -12,12 +25,14 @@ def cart_view(request):
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer= customer, complete= False)
         items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
     else:
         item = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
     context = {
         'items' : items,
         'order' : order ,
+        'cartitems' : cartitems
     }
     return render(request, 'store/cart.html', context)
 
@@ -28,6 +43,7 @@ def checkout_view(request):
         customer = request.user.customer
         order,created = Order.objects.get_or_create(customer= customer, complete= False)
         items = order.orderitem_set.all()
+        cartitems = order.get_cart_items
     else:
         item = []
         order = {'get_cart_total': 0, 'get_cart_items': 0}
@@ -36,3 +52,23 @@ def checkout_view(request):
         'order' : order ,
     }
     return render(request, 'store/checkout.html', context)
+
+def updateitem_view (request):
+    data = json.loads(request.data)
+    productId = data['productId']
+    action = data['action']
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order,created = Order.objects.get_or_create(order= order, complete= False)
+    cartitems = order.get_cart_items
+    
+    if action == 'add' :
+        cartitems =  cartitems + 1
+    elif action == 'remove':
+       cartitems =  cartitems - 1
+    OrderItem.save()
+    
+    if OrderItem.quantity <= 0 :
+        OrderItem.delete()
+        
+    return JsonResponse('Item was added', safe = False)
